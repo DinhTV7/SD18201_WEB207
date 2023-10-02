2b183a9af34f123303792a4e119d6ff9f827abb2
Ta Van Dinh <dinhtv7@fpt.edu.vn>
2023-10-02 18:03:20 +0700

    update

--- route/controllers/GioiThieuController.js
+++ route/controllers/GioiThieuController.js
@@ -2,4 +2,29 @@ window.GioiThieuController = function ($scope, $routeParams) {
     $scope.title = "Đây là trang giới thiệu";
     let id = $routeParams.id;
     console.log(id);
+
+    // Khai báo mảng
+    $scope.students = [
+        {
+            ten: "Tạ Văn Định",
+            namsinh: 2004,
+            chuyennganh: "CNTT"
+        },
+        {
+            ten: "DinhTV7",
+            namsinh: 2004,
+            chuyennganh: "CNTT"
+        }
+    ];
+
+    $scope.createStudent = function () {
+        // Lấy dữ liệu từ input
+        let newStudent = {
+            ten: $scope.student.name,
+            namsinh: $scope.student.year,
+            chuyennganh: $scope.student.couse
+        }
+        // Thêm thông tin nhập tư ô inout vào mảng
+        $scope.students.push(newStudent);
+    }
 }
--- route/controllers/TrangChuController.js
+++ route/controllers/TrangChuController.js
@@ -1,3 +1,30 @@
 window.TrangChuController = function ($scope) {
     $scope.title = "Đây là trang chủ";
+
+    $scope.themMoi = function () {
+        // Hiển thị toàn bộ dữ liệu form đẩy vào
+        console.log($scope.inputValue);
+        // Hiển thị chi tiết
+        console.log($scope.inputValue.name);
+        console.log($scope.inputValue.address);
+    }
+
+    $scope.tinhTong = function () {
+        $scope.sum = parseFloat($scope.number.one) + parseFloat($scope.number.two);
+    }
+
+    $scope.checkLogin=function(){
+        $scope.logged = false;
+        if ($scope.user.pass == "thaydinhdz") {
+            $scope.logged = true;
+        }
+        else{
+            alert("Bạn nhập sai mật khẩu")
+        }
+    }
+
+    $scope.count = 0;
+    $scope.countClick = function () {
+        $scope.count++;
+    }
 }
